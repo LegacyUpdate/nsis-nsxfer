033c0a7db0b555765f1bff72fa5cb54907dea2c9
Douglas R. Reno <renodr2002@gmail.com>
2026-01-23 16:14:23 -0600

    Restore the previous Makefile.mingw and _build_Release_mingw.bat files.

--- _build_Release_mingw.bat
+++ _build_Release_mingw.bat
@@ -1,57 +1,64 @@
 REM :: Marius Negrutiu (marius.negrutiu@protonmail.com)
 
 @echo off
-setlocal EnableDelayedExpansion
+echo.
 
-if "%config%" equ "" set config=%~1
-if "%config%" equ "" set config=Release
+if exist "C:\cygwin64\bin\i686-w64-mingw32-gcc.exe"   set PREFIX32=i686-w64-mingw32-
+if exist "C:\cygwin64\bin\x86_64-w64-mingw32-gcc.exe" set PREFIX64=x86_64-w64-mingw32-
 
-set makemore=%~2
+if not exist "%MINGW32%\bin\%PREFIX32%gcc.exe" set MINGW32=%MINGW32_INSTDIR%
+if not exist "%MINGW32%\bin\%PREFIX32%gcc.exe" set MINGW32=%SYSTEMROOT%\msys64\mingw32
+if not exist "%MINGW32%\bin\%PREFIX32%gcc.exe" set MINGW32=%SYSTEMROOT%\msys2\mingw32
+if not exist "%MINGW32%\bin\%PREFIX32%gcc.exe" set MINGW32=C:\cygwin64
 
-for %%d in ("%MINGW32_INSTDIR%" %SYSTEMDRIVE%\mingw32 %SYSTEMDRIVE%\msys64\mingw32 "") do (
-  if not exist "!MINGW32!\bin\gcc.exe" set MINGW32=%%~d
-)
-for %%d in ("%MINGW64_INSTDIR%" %SYSTEMDRIVE%\mingw64 %SYSTEMDRIVE%\msys64\mingw64 "") do (
-  if not exist "!MINGW64!\bin\gcc.exe" set MINGW64=%%~d
-)
-for %%d in (%SYSTEMDRIVE%\msys64\usr\bin %SYSTEMDRIVE%\cygwin64\bin "%PROGRAMFILES%\Git\usr\bin" "") do (
-  if not exist "!posix_shell!\grep.exe" set posix_shell=%%~d
-)
+if not exist "%MINGW64%\bin\%PREFIX64%gcc.exe" set MINGW64=%MINGW64_INSTDIR%
+if not exist "%MINGW64%\bin\%PREFIX64%gcc.exe" set MINGW64=%SYSTEMROOT%\msys64\mingw64
+if not exist "%MINGW64%\bin\%PREFIX64%gcc.exe" set MINGW64=%SYSTEMROOT%\msys2\mingw64
+if not exist "%MINGW64%\bin\%PREFIX64%gcc.exe" set MINGW64=C:\cygwin64
+
+if exist "%MINGW32%\bin\mingw32-make.exe" set MAKE="%MINGW32%\bin\mingw32-make.exe"
+if exist "%MINGW32%\bin\make.exe"         set MAKE="%MINGW32%\bin\make.exe"
+if not exist "%MAKE%" echo ERROR: Missing "%MINGW32%\bin\mingw32-make.exe" && pause && exit /B 2
 
 set ORIGINAL_PATH=%PATH%
 
 cd /d "%~dp0"
 
 :x86
-set PATH=%MINGW32%\bin;%posix_shell%;%ORIGINAL_PATH%
+if not exist "%MINGW32%\bin\%PREFIX64%gcc.exe" echo ERROR: Missing "%MINGW32%" && pause && exit /B 2
+set PATH=%MINGW32%\bin;%ORIGINAL_PATH%
 
 echo.
 echo -------------------------------------------------------------------
-set OUTDIR=build\%config%-mingw-x86-ansi
+set OUTDIR=Release-mingw-x86-ansi
 echo %OUTDIR%
 title %OUTDIR%
 echo -------------------------------------------------------------------
-mingw32-make.exe ARCH=X86 CHAR=ANSI CONFIG=%config% OUTDIR=%OUTDIR% -fMakefile.mingw clean all %makemore% || pause && exit /b !errorlevel!
+%MAKE% ARCH=X86 CHAR=ANSI OUTDIR=%OUTDIR% -f Makefile.mingw clean all
+if %errorlevel% neq 0 pause && exit /B %errorlevel%
 
 echo.
 echo -------------------------------------------------------------------
-set OUTDIR=build\%config%-mingw-x86-unicode
+set OUTDIR=Release-mingw-x86-unicode
 echo %OUTDIR%
 title %OUTDIR%
 echo -------------------------------------------------------------------
-mingw32-make.exe ARCH=X86 CHAR=Unicode CONFIG=%config% OUTDIR=%OUTDIR% -fMakefile.mingw clean all %makemore% || pause && exit /b !errorlevel!
+%MAKE% ARCH=X86 CHAR=Unicode OUTDIR=%OUTDIR% -f Makefile.mingw clean all
+if %errorlevel% neq 0 pause && exit /B %errorlevel%
 
 
 :amd64
-set PATH=%MINGW64%\bin;%posix_shell%;%ORIGINAL_PATH%
+if not exist "%MINGW64%\bin\%PREFIX64%gcc.exe" echo ERROR: Missing "%MINGW64%" && pause && exit /B 2
+set PATH=%MINGW64%\bin;%ORIGINAL_PATH%
 
 echo.
 echo -------------------------------------------------------------------
-set OUTDIR=build\%config%-mingw-amd64-unicode
+set OUTDIR=Release-mingw-amd64-unicode
 echo %OUTDIR%
 title %OUTDIR%
 echo -------------------------------------------------------------------
-mingw32-make.exe ARCH=X64 CHAR=Unicode CONFIG=%config% OUTDIR=%OUTDIR% -fMakefile.mingw clean all %makemore% || pause && exit /b !errorlevel!
+%MAKE% ARCH=X64 CHAR=Unicode OUTDIR=%OUTDIR% -f Makefile.mingw clean all
+if %errorlevel% neq 0 pause && exit /B %errorlevel%
 
 echo.
 REM pause
